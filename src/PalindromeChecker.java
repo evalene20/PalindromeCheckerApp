f3a87c2dfd9a2a7d7a4eb48918bbd6961b0c58db
evalene20 <aklene20@gmail.com>
2026-03-08 11:15:26 +0530

    UC4 implementation

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,11 +1,19 @@
-public class PalindromeChecker {
-    public static void main (String[] args)
-    {
-        uc4_CharArrayMethod();
-    }
-    public static void uc4_CharArrayMethod() {
+import java.util.Scanner;
 
-        String input = "racecar";
+public class PalindromeChecker {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a word to check: ");
+        String input = scanner.nextLine();
+
+        uc4_CharArrayMethod(input);
+
+        scanner.close();
+    }
+
+    public static void uc4_CharArrayMethod(String input) {
+
         char[] chars = input.toCharArray();
 
         int start = 0;
@@ -26,6 +34,5 @@ public class PalindromeChecker {
         System.out.println("UC4: Character Array Based Palindrome Check");
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
-        System.out.println();
     }
 }
